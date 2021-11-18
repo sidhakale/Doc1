3aa041f90ed3107e466256c495c427c584bd0b05
sidhakale <94164974+sidhakale@users.noreply.github.com>
2021-11-18 11:57:06 +0530

    Update aws-instance-example.tf
    Updated changes

--- aws-instance-example.tf
+++ aws-instance-example.tf
@@ -2,9 +2,9 @@ resource "aws_instance" "web1" {
    ami           = "ami-0c2b8ca1dad447f8a"
    instance_type = "t2.micro"
    count = 1
-  vpc_security_group_ids = ["sg-04167b4767dbd2890"]
-   key_name               = "key-0cfa7d3e2d98a815e" 
-   iam_instance_profile =   "EC2"
+  vpc_security_group_ids = ["sg-09a92b159eb276c66"]
+   key_name               = "KeyPairNV" 
+   iam_instance_profile =   "EC2Role"
    user_data = <<-EOF
       #!/bin/bash     
       sudo amazon-linux-extras install ansible2 -y
