47304d9e5b3f8e00e64e458ad0da17340b5d28cd
sidhakale <94164974+sidhakale@users.noreply.github.com>
2021-11-18 14:47:16 +0530

    Update aws-instance-example.tf

--- aws-instance-example.tf
+++ aws-instance-example.tf
@@ -2,8 +2,7 @@ resource "aws_instance" "web1" {
    ami           = "ami-0c2b8ca1dad447f8a"
    instance_type = "t2.micro"
    count = 1
-  vpc_security_group_ids = ["vpc-0e69561ef38533cea"]
-   security_groups = ["sg-09a92b159eb276c66"]
+   vpc_security_group_ids = ["sg-09a92b159eb276c66"]
    key_name               = "KeyPairNV" 
    iam_instance_profile =   "EC2Role"
    user_data = <<-EOF
@@ -11,7 +10,7 @@ resource "aws_instance" "web1" {
       sudo amazon-linux-extras install ansible2 -y
       sudo yum install git -y
       sudo yum install maven -y      
-      git -C ./home/ec2-user clone https://github.com/Abishek-Ravichander/Terra-Ansi-jFrog.git          
+      git -C ./home/ec2-user clone https://github.com/sidhakale/Doc1.git          
       cd /home/ec2-user/Terra-Ansi-jFrog && ansible-playbook main.yml -f 10
       cd /home/ec2-user/Terra-Ansi-jFrog && wget -O Java-Ansible.war https://abishek.jfrog.io/artifactory/abi_new/MyMavanWebapp/Java-Ansible/0.0.1-SNAPSHOT/Java-Ansible-0.0.1-SNAPSHOT.war
       cd /home/ec2-user/Terra-Ansi-jFrog && ansible-playbook deploy.yml -f 10
